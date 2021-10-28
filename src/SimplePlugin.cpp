1289c1e05469afd3958a0379b5f194b0af2feddd
jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
2021-10-28 18:02:12 +0100

    Set up simple gain plugin

--- src/SimplePlugin.cpp
+++ src/SimplePlugin.cpp
@@ -1,15 +1,31 @@
 #include "SimplePlugin.h"
 
+namespace
+{
+const String gainParamTag = "gain";
+}
+
 SimplePlugin::SimplePlugin()
 {
+    gainParam = vts.getRawParameterValue (gainParamTag);
 }
 
 void SimplePlugin::addParameters (Parameters& params)
 {
+    using namespace chowdsp::ParamUtils;
+    params.push_back (std::make_unique<VTSParam> (gainParamTag,
+                                                  "Gain",
+                                                  String(),
+                                                  NormalisableRange { -12.0f, 12.0f },
+                                                  0.0f,
+                                                  gainValToString,
+                                                  stringToGainVal));
 }
 
 void SimplePlugin::prepareToPlay (double sampleRate, int samplesPerBlock)
 {
+    gain.prepare ({ sampleRate, (uint32) samplesPerBlock, (uint32) getMainBusNumInputChannels() });
+    gain.setRampDurationSeconds (0.05);
 }
 
 void SimplePlugin::releaseResources()
@@ -18,6 +34,20 @@ void SimplePlugin::releaseResources()
 
 void SimplePlugin::processAudioBlock (AudioBuffer<float>& buffer)
 {
+    dsp::AudioBlock<float> block { buffer };
+    dsp::ProcessContextReplacing<float> context { block };
+    gain.process (context);
+}
+
+AudioProcessorEditor* SimplePlugin::createEditor()
+{
+    auto builder = chowdsp::createGUIBuilder (magicState);
+    auto editor = new foleys::MagicPluginEditor (magicState, BinaryData::gui_xml, BinaryData::gui_xmlSize, std::move (builder));
+
+    // we need to set resize limits for StandalonePluginHolder
+    editor->setResizeLimits (10, 10, 2000, 2000);
+
+    return editor;
 }
 
 // This creates new instances of the plugin
--- src/SimplePlugin.h
+++ src/SimplePlugin.h
@@ -13,6 +13,12 @@ public:
     void releaseResources() override;
     void processAudioBlock (AudioBuffer<float>& buffer) override;
 
+    AudioProcessorEditor* createEditor() override;
+
 private:
+    std::atomic<float>* gainParam = nullptr;
+
+    dsp::Gain<float> gain;
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimplePlugin)
 };
